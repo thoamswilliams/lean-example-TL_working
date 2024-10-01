75a51cbbbb34beef3d13c68a509f0368506cb44d
Manooshree Patel <manooshreepatel@berkeley.edu>
2024-10-01 13:42:32 -0700

    Update Example.lean

--- Example.lean
+++ Example.lean
@@ -1,3 +1,19 @@
 -- This module serves as the root of the `Example` library.
 -- Import modules here that should be built as part of the library.
-import Example.Basic
+import Mathlib.LinearAlgebra.Eigenspace.Basic
+
+
+
+variable {R n M : Type*} [DecidableEq n] [Fintype n]
+
+open Matrix Module.End
+
+section NontrivialCommRing
+
+variable [CommRing R] [Nontrivial R] [AddCommGroup M] [Module R M]
+
+
+/-- Basis vectors are eigenvectors of associated diagonal linear operator. -/
+lemma hasEigenvector_toLin_diagonal (d : n → R) (i : n) (b : Basis n R M) :
+  HasEigenvector (toLin b b (diagonal d)) (d i) (b i) :=
+  ⟨mem_eigenspace_iff.mpr <| by simp [diagonal], Basis.ne_zero b i⟩
